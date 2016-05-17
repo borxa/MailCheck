d048e2202be40d5d391e66414f6d255175db705c
borxa <borxa@www.a-revolta.es>
2016-05-17 22:32:24 +0200

    Add first test class

---
+++ src/test/java/net/borxa/mailcheck/util/MailCheckUtilTest.java
@@ -0,0 +1,95 @@
+package net.borxa.mailcheck.util;
+
+import javax.mail.Message;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Borxa Varela
+ */
+public class MailCheckUtilTest {
+    
+    public MailCheckUtilTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getContentMessage method, of class MailCheckUtil.
+     */
+    @Test
+    public void testGetContentMessage() {
+//        System.out.println("getContentMessage");
+//        Message message = null;
+//        String expResult = "";
+//        String result = MailCheckUtil.getContentMessage(message);
+//        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of notifyNewUnreadMessages method, of class MailCheckUtil.
+     */
+    @Test
+    public void testNotifyNewUnreadMessages() throws Exception {
+//        System.out.println("notifyNewUnreadMessages");
+//        String host = "";
+//        String user = "";
+//        String password = "";
+//        MailCheckUtil.notifyNewUnreadMessages(host, user, password);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of removeMessage method, of class MailCheckUtil.
+     */
+    @Test
+    public void testRemoveMessage() throws Exception {
+//        System.out.println("removeMessage");
+//        long messageId = 0L;
+//        String host = "";
+//        String user = "";
+//        String password = "";
+//        boolean expResult = false;
+//        boolean result = MailCheckUtil.removeMessage(messageId, host, user, password);
+//        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMessageId method, of class MailCheckUtil.
+     */
+    @Test
+    public void testGetMessageId() {
+//        System.out.println("getMessageId");
+//        Message message = null;
+//        long expResult = 0L;
+//        long result = MailCheckUtil.getMessageId(message);
+//        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
